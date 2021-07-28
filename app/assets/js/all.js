4892be4b8d0901cee0f5c72d011b26521f0e1ab7
熊正奇 <as7196528@gmail.com>
2021-07-28 14:18:37 +0800

    add product.html

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -1,5 +1,21 @@
-console.log('Hello!');
-
 $(document).ready(() => {
-  console.log('HesSchool Hello!');
+  
+  $('.tab-link').click(function(){
+    //切換active
+    $('.tab').removeClass('active');
+    $(this).parent().addClass('active');
+
+    let currentTab = $(this).attr('href');
+    $('.content>div').hide();
+    $(currentTab).show();
+
+    return false;
+  });
+
+  $('.pagination-item').click(function(e){
+    e.preventDefault();
+    $('.pagination-item').removeClass('active');
+    $(this).addClass('active');
+  });
+
 });
